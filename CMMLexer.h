b30a06a51a9b925fc0601452c6e6e5335449dd68
Wang Hsutung <xukiro@outlook.com>
2016-10-20 17:40:07 +0800

    +cmm::Token::TokenKind

--- CMMLexer.h
+++ CMMLexer.h
@@ -6,4 +6,39 @@
 
 void show(const std::string &s);
 
+namespace cmm {
+
+class Token {
+public:
+  enum TokenKind {
+    // String values.
+    Identifier,
+    String,
+
+    // Integer values.
+    Int,
+
+    // Real values.
+    Double,
+
+    // Boolean value.
+    Bool,
+
+    // No-values.
+    LParen, RParen, LBrac, RBrac, LCurly, RCurly,
+    Plus, Minus, Star, Slash, Semicolon, Comma,
+    Equal, Percent, Exclaim, AmpAmp, PipePipe,
+    Less, LessEqual, EqualEqual, Greater, GreaterEqual, ExclaimEqual,
+    Amp, Pipe, LessLess, GreaterGreater, Caret, Tilde,
+
+    // Flow Control keywords
+    If, Else, For, While, Do, Break, Continue
+  };
+
+private:
+  TokenKind Kind;
+};
+
+}
+
 #endif
